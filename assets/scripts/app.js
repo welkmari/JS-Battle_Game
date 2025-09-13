801beee26d9a56c916bd711d2c004b8092993579
Maria Helena Welk <welktech46@gmail.com>
2025-09-13 09:14:26 -0300

    commit

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -1,5 +1,5 @@
 const ATTACK_VALUE = 10;
-const STRONG_ATTACK_VALUE = 
+const STRONG_ATTACK_VALUE = 17;
 const MONSTER_ATTACK_VALUE = 14;
 
 let chosenMaxLife = 100;
@@ -8,8 +8,14 @@ let currentPlayerHealth = chosenMaxLife;
 
 adjustHealthBars(chosenMaxLife);
 
-function attackHandler() {
-    const damage = dealMonsterDamage(ATTACK_VALUE);
+function attackMonster(mode) {
+    let maxDamage;
+    if (mode == 'ATTACK') {
+        maxDamage = ATTACK_VALUE; 
+    } else if(mode =='STRONG_ATTACK') {
+        maxDamage = STRONG_ATTACK_VALUE;
+    }
+    const damage = dealMonsterDamage(maxDamage);
     currentMonsterHealth -= damage;
     const playerDamage = dealPlayerDamage(MONSTER_ATTACK_VALUE);
     currentPlayerHealth -= playerDamage;
@@ -22,20 +28,14 @@ function attackHandler() {
     }
 }
 
+function attackHandler() {
+    attackMonster('ATTACK');
+}
+
 function strongAttackHandler() {
-    const damage = dealMonsterDamage(ATTACK_VALUE);
-    currentMonsterHealth -= damage;
-    const playerDamage = dealPlayerDamage(MONSTER_ATTACK_VALUE);
-    currentPlayerHealth -= playerDamage;
-    if (currentMonsterHealth <=0 && currentPlayerHealth > 0) {
-        alert('YOU WON!');
-    } else if(currentPlayerHealth <= 0 && currentPlayerHealth > 0) {
-        alert('YOU LOST!')
-    } else if(currentPlayerHealth <=0 && currentMonsterHealth <=0 && currentMonsterHealth <=0 ) {
-        alert('YOU HAVE A DRAW!')
-    }
+   attackMonster('STRONG_ATTACK');
 }
     
 attackBtn.addEventListener('click', attackHandler);
-strongAttackBtn.addEventListener('click');
+strongAttackBtn.addEventListener('click',strongAttackHandler);
 
